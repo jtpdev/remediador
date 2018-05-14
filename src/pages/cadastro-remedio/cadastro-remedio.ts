db1292f73627b47e1ab28e574805f68682512a29
Jimmy Porto <ireturnerd@gmail.com>
2018-05-14 14:34:25 -0300

    Added android platform

--- src/pages/cadastro-remedio/cadastro-remedio.ts
+++ src/pages/cadastro-remedio/cadastro-remedio.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Remedio } from '../../models/remedio';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 
 @IonicPage()
 @Component({
@@ -13,9 +14,20 @@ export class CadastroRemedioPage {
 
   constructor(
     public navCtrl: NavController,
-    public navParams: NavParams
+    public navParams: NavParams,
+    private barcodeScanner: BarcodeScanner
   ) {
     this.remedio = new Remedio();
+  }
+
+  chamarScanner() {
+    if (!this.remedio.codigoBarras) {
+      this.barcodeScanner.scan().then(codigo => {
+        this.remedio.codigoBarras = Number(codigo.text);
+      }).catch(err => {
+        console.log('Erro ao escanear o código', err);
+      });
+    }
   }
 
 }
